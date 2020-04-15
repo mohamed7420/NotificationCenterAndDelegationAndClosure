cb28177a78d1f346695361e4dc085e91c7c70ce6
mohamedosama2074 <iosengks74@gmail.com>
2020-04-15 03:05:38 +0200

    Initial Commit

---
+++ NotificationCenterAndDelegationAndClosure/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  NotificationCenterAndDelegationAndClosure
+//
+//  Created by Mohamed on 4/15/20.
+//  Copyright © 2020 Mohamed74. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
